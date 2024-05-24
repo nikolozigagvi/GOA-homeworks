fef789ba09068cfdae8c4bc691b415a20a23dcbb
nikolozigagvi <ngagvishvili2012@gmail.com>
2024-05-25 00:41:57 +0400

    homework

---
+++ level 03/homework/level03.py
@@ -0,0 +1,109 @@
+name = 'nikoloz'
+print(name)
+
+last_name = 'gagvishvili'
+print(last_name)
+
+age = 12
+print(age)
+
+hobby = "Swimming , Karate"
+print(hobby)
+
+country = "Georgia"
+print(country)
+
+job = "developer"
+level = 3
+working = "super hard"
+
+print(job , level , working)
+
+#მიმატება
+print(10 + 100020)
+print(24 + 30)
+print(45 + 605)
+print(78 + 586)
+print(2000010  +73893)
+print(74883 + 90)
+print(100 + 748)
+print(39029 + 98222)
+print(7588 + 6464)
+print(34776 + 3736)
+
+#გამოკლება
+print(3232332 -223)
+print(323345666 - 666)
+print(-22232 - 607)
+print(450 - 3344)
+print(-2 - 3)
+print( 9955 - 443)
+print(23245 - 5567)
+print(234677 - 344566)
+print(2356734 - 466567664)
+print(3-46778786)
+
+#გამრავლება
+print(223 * 2)
+print(5 * 5)
+print(43434345787423 * 2)
+print(45467 * 4465)
+print(35 * 56)
+print(12 * 34)
+print(60 * 2)
+print(84  * 4)
+print(3435 * 4)
+print(54 * 4)
+
+#გაყოფა
+print(23 // 4)
+print(433 // 5)
+print(304 // 6)
+print(709 // 3)
+print(3 // 5)
+print(4544654 // 4)
+print(4 // 5)
+print(355 // 6)
+print(666 // 6)
+print(103854775483 // 4454)
+
+#მეორე გაყოფა (ათწილადი)
+print(2223 / 344)
+print(454657 /43543)
+print(20 / 3)
+print(47833 / 22)
+print(4333 / 334)
+print(232472 / 445)
+print(348 / 234)
+print(356543 / 35676)
+print(1111111 / 7)
+print(3455 /76899)
+
+#ხარისხები
+print(24554 ** 4)
+print(456 ** 6)
+print(245 ** 56)
+print(245 ** 5)
+print(2324 **  45)
+print(2445 **  48)
+print(28874 ** 32)
+print(676 ** 28)
+print(6998 **  5)
+print(46575 ** 67)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
