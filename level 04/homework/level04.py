bb7baa01f309aca2b0e362ff5589280421c6c733
nikolozigagvi <ngagvishvili2012@gmail.com>
2024-06-03 01:38:26 +0400

    second commit

---
+++ level 04/homework/level04.py
@@ -0,0 +1,147 @@
+#მიმატება
+
+print(20 + 3)
+print(110 + 5)
+print(298 + 56)
+print(87 + 75)
+print(232 + 5667)
+print(459 + 56)
+print(2012 + 12)
+print(2012 + 4785 + 45 )
+print(68773 + 5465 + 667 +667 + 345 )
+print(36 + 55)
+print(279 + 556)
+print(54 + 5)
+print(456 + 67)
+print(4564 + 56874476374634536463743)
+print(764 + 56)
+
+#გამოკლება
+
+print(67 - 8)
+print(565 - 44)
+print(7734 - 565)
+print(3345 - 767)
+print(556 - 32)
+print(8663 - 67)
+print(224567 -6757)
+print(4334 - 754)
+print(8455- 354)
+print(24345 - 56)
+print(35 -7)
+print(34 - 4)
+print(355 -778)
+print(224 - 56)
+print(9874 - 455)
+
+#გამრავლება
+
+print(56 * 4565)
+print(333 * 11)
+print(3 * 556)
+print(66 * 55)
+print(45 * 556)
+print(22 * 56)
+print(11 * 5)
+print(667 * 55)
+print(213 * 6)
+print(233 * 5)
+print(2 * 6)
+print(223 * 76)
+print(354 * 543)
+print(112 * 122)
+print(344 * 44)
+
+#გაყოფა
+
+print(2933 // 4)
+print(11 // 43)
+print(33 // 11)
+print(54 // 6)
+print(334 // 56)
+print(999 // 11)
+print(333 // 45)
+print(5456 // 76)
+print(7744 // 6)
+print(77 // 11)
+print(33334 // 66)
+print(23443 // 5 )
+print(59975 //  5)
+print(4442 // 2)
+print(765 // 2)
+
+#ხარისხები
+
+print(543 ** 5)
+print(55 ** 1)
+print(33 ** 0)
+print(455 ** 4)
+print(994 ** 55)
+print(255 ** 4)
+print(112 ** 4)
+print(57 ** 33)
+print(447 ** 4)
+print(34 ** 5)
+print(22 ** 54)
+print(344 ** 4)
+print(555 ** 2)
+print(32 ** 34)
+print(343 ** 4)
+
+name = "gega"
+last_name = "gelashvili"
+iq = 90
+job = "developer , fixing bugs"
+happiness = "100%"
+
+print(name)
+print(last_name)
+print(iq)
+print(job)
+print(happiness)
+
+name = "giorgi"
+last_name = "mamardashvili"
+iq = 100
+job = "playing football"
+happiness = "87%"
+
+print(name)
+print(last_name)
+print(iq)
+print(job)
+print(happiness)
+
+game_visits = 10000000
+game_likes = 50000
+game_FPS = 60
+game_interest = 1000000
+
+print(game_visits)
+print(game_likes)
+print(game_FPS)
+print(game_interest)
+      
+name = "lionel"
+last_name = "andres messi"
+goals = 806
+assist = 450
+secsessfull = "100%"
+
+print(name)
+print(last_name)
+print(goals)
+print(assist)
+print(secsessfull)
+
+
+video = 5
+print(video + 43)
+
+homework = 1
+coding = 10
+print(homework + coding)
+
+
+
+
