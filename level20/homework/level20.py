d6745235a1522735201d5c1c33d9bad3d16f8d96
nikolozigagvi <ngagvishvili2012@gmail.com>
2024-10-03 20:01:09 +0400

    second commit

---
+++ level20/homework/level20.py
@@ -0,0 +1,18 @@
+values = ["text", "number", "text2" , "text4" "no text3?" , "hi" , "hi3" , "no hi3?" , "just jk" ,"hi2" ,"hi5" , "hifiive:) ","good ok?"]
+print(values[3])
+print(values[5])
+print(values[9])
+print(values[6])
+
+values[1] = "no text 6 he is sad"
+values[4] = "no text 8 he is sad"
+values[5] = "no text 9 he is sad"
+values[11] = "no text 3 he is sad"
+values[7] = "no text 13 he is sad"
+values[6] = "no text 10 he is sad"
+values[3] = "no text 4 he is sad"
+values[2] = "no text 0 he is sad"
+
+
+print(values + ["heloo ," "and bye" , "hiiiiiiiiiii"])
+print(values)
