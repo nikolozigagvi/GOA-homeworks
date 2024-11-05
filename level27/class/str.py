b5fad11ef5360ce46b55a67c9338707d532cfa54
nikolozigagvi <ngagvishvili2012@gmail.com>
2024-11-05 23:55:20 +0400

    second commit

---
+++ level27/class/str.py
@@ -0,0 +1,21 @@
+#dav 1
+txt = "hello#how#are#you?"
+
+new_txt = txt.replace('#', ' ')
+
+print(new_txt)
+
+#dav4
+a = "Spam"
+b = a.upper()
+print(b)
+
+#dav3
+
+x = str.split('!')
+new_str = str.split('.')
+print(x)
+
+muhamed = input("enter how much muhamed :")
+if muhamed >34:
+    print('yes love muhamed')
---
+++ level27/home/work.py
@@ -0,0 +1,38 @@
+#2
+name = 'alice'
+place = "Wonderland"
+template = f'hello {name} welcome to {place}'
+print(template)
+
+
+#3
+fruit = "apple", "banana", "cherry"
+fruit = ", ".join(["apple" "banana" "cherry"])
+print(fruit)
+
+
+
+#4
+sentence = "The quick brown fox jumps over the lazy dog."
+new_sec = sentence.split(" ")
+print(new_sec)
+
+
+
+#5
+quote =  "To be or not to be, that is the question."
+new_quote = quote.replace("be" , "code")
+print(new_quote)
+
+
+#6
+mixed_case =  "PyThOn Is AwEsOmE!"
+nw_mix_case = mixed_case.lower()
+print(nw_mix_case)
+
+
+
+#7
+greeting = "good morning"
+uppercase_greeting = greeting.upper()
+print(uppercase_greeting)
