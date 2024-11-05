5fcec6804c26d6b7059c0321643a7ada914d09b7
nikolozigagvi <ngagvishvili2012@gmail.com>
2024-11-06 00:00:36 +0400

    second commit

---
+++ level25/homework/homework.py
@@ -0,0 +1,31 @@
+# dav 1if m<0 or n<0:
+def paperwork(n, m):
+    if m<0 or n<0:
+        print(0) 
+    else:
+        print( n*m)
+
+#dav 2
+def quarter_of(month):
+    
+    if month <= 3:
+        return 1
+        
+    elif month <=6 and month > 3:
+        return 2
+        
+    elif month <=9 and month > 6:
+        return 3
+        
+    else:
+        return 4
+    
+
+# dav 3
+ 
+    return "yes"  if boolean else "No"
+
+#dav 4
+
+def greet(name):
+    return "Hello, " + name + " how are you doing today?"
