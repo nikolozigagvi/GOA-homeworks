da8cfc953ada616e1797c0a94e0de12b6b211f67
nikolozigagvi <ngagvishvili2012@gmail.com>
2024-12-12 20:34:30 +0400

    second commit

---
+++ level32/home/py.py
@@ -0,0 +1,33 @@
+ #def human_years_cat_years_dog_years(human_years):
+    #catYears = 0
+    #dogYears = 0
+    #if human_years == 1:
+        #catYears += 15
+        #dogYears += 15
+        #return [human_years, catYears, dogYears]
+    #elif
+        #human_years < 2
+        #catYears +=24
+        #dogYears +=24
+        #return [human_years, catYears, dogYears]
+    #elif human_years > 2:
+        #catYears += 24
+        #dogYears += 24
+        #years = human_years - 2
+        #catYears += years*4
+        #dogYears += years*5
+        #return [human_years, catYears, dogYears]
+    #return [0,0,0]
+
+
+
+
+#def solution(string):
+    #stringa = ""
+    #for i in string:
+        #string = stringa + i
+        #return stringa
+  
+
+
+
