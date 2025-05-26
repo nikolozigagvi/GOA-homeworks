8490920a05c12f2b00a22744f6bad93818061004
nikolozigagvi <ngagvishvili2012@gmail.com>
2025-05-26 10:57:16 +0400

    second commit

---
+++ level54/class/js.js
@@ -0,0 +1,15 @@
+function formsubmit(e){
+    e.preventdefult();
+
+let nameint = document.getElementById("name-inp")
+let surnameint = document.getElementById("surname-inp")
+let passwordint = document.getElementById("password-inp")
+}
+
+console.log(name-int)
+console.log(surname-int)
+console.log(passwordint-int)
+
+nameint.value = ""
+surnameintnameint.value = ""
+passwordintint.value = ""
