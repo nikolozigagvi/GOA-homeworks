6f41a46dd1b6e941aa4d4217b3b8bf7f3474ff03
nikolozigagvi <ngagvishvili2012@gmail.com>
2025-06-14 20:49:07 +0400

    second commit

---
+++ level56/home/js.js
@@ -0,0 +1,35 @@
+function checkiftrueotfalseyoudump(){
+    let input = document.getElementById("input1")
+
+    console.log(input.checked)
+}
+
+console.log(10 > 2)
+
+console.log(10 < 50)
+
+console.log(10 == 36472642367423)
+
+console.log(10 != 36472642367423)
+
+console.log(10 >= 33)
+
+
+function truingorfalsing(){
+    let button = document.getElementById("but")
+
+    console.log(true)
+
+    console.log(false)
+}
+
+function resentonsyfm(e){
+    e.preventDefult()
+
+let name = document.getElementById("inputting")
+name.textContent = email
+
+console.log(name)
+
+form.reset()
+}
