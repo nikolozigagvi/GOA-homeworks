ca8197720e981c5b2f9c18f21f9fb5c8bbcd0dde
nikolozigagvi <ngagvishvili2012@gmail.com>
2025-07-13 01:21:35 +0400

    second commit

---
+++ level62/class/js.js
@@ -0,0 +1,13 @@
+document.getElementById(button)
+function changebackgroundcolor(){
+    button.style.backGround = "black"
+}
+let h2 = document.getElementById("h2-main")
+function jss(){
+    let num = Number(prompt("enter your num:"))
+    document.getElementById("h2-main")
+    h2.style.font-size == num + "px"
+}
+
+let src = document.getElementById("img1")
+console.log("img1.src")
---
+++ level62/home/js.js
@@ -0,0 +1,22 @@
+let button = document.getElementById("button1")
+
+function change(){
+    TextContent = prompt("enter you textcontent :")
+}
+
+let button2 = document.getElementById("button2")
+
+function div2(){
+    div2.style.BackgroundColor = "green"
+}
+
+let button3 = document.getElementById("button3")
+function div3(){
+    div3.style.font-size == "small"
+}
+
+let button4 = document.getElementById("button4")
+function div4(){
+    div4.style.Display = "none"
+}
+
